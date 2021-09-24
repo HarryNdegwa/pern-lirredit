45358d151d1cfb6e7693978f9129c55c3cae6496
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-09-24 12:58:19 +0300

    Applied migrations for user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field()
+  @PrimaryKey()
+  id!: number;
+
+  @Field()
+  @Property({ unique: true })
+  username!: string;
+
+  @Property()
+  password!: string;
+
+  @Field()
+  @Property({ type: "date" })
+  createdAt: Date = new Date();
+
+  @Field() // Exposes this field in the schema
+  @Property({ onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+}
