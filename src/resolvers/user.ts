79a58e2f6c8bad50b8fbe81404b58f33cbff7323
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-09-26 00:48:06 +0300

    Separated key

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -211,7 +211,9 @@ export class UserResolver {
         ],
       };
     }
-    const userId = await redis.get(FORGET_PASSWORD_PREFFIX + token);
+
+    const key = FORGET_PASSWORD_PREFFIX + token;
+    const userId = await redis.get(key);
 
     if (!userId) {
       return {
@@ -239,6 +241,8 @@ export class UserResolver {
     user.password = await argon2.hash(password);
 
     em.persistAndFlush(user);
+
+    await redis.del(key);
 
     // optional
     req.session.UserID = user.id;
