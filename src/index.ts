81c30084a44e9d2a4cb4088babcfd7afa545085f
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-09-25 17:31:49 +0300

    Added logout resolver

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
@@ -10,6 +10,7 @@ import { UserResolver } from "./resolvers/user";
 import redis from "redis";
 import session from "express-session";
 import connectRedis from "connect-redis";
+import cors from "cors";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -20,8 +21,15 @@ const main = async () => {
   const redisClient = redis.createClient();
 
   app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+
+  app.use(
     session({
-      name: "qid",
+      name: COOKIE_NAME,
       store: new RedisStore({ client: redisClient, disableTouch: true }),
       cookie: {
         maxAge: 315576000000,
@@ -42,7 +50,10 @@ const main = async () => {
     context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
     console.log("Started server...");
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
 } from "type-graphql";
 import { User } from "../entities/User";
 import argon2 from "argon2";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
@@ -129,28 +130,19 @@ export class UserResolver {
     return { user };
   }
 
-  //   @Mutation(() => Post, { nullable: true })
-  //   async updatePost(
-  //     @Arg("id") id: number,
-  //     @Arg("title") title: string,
-  //     @Ctx() ctx: MyContext
-  //   ): Promise<Post | null> {
-  //     const post = await ctx.em.findOne(Post, { id });
-  //     if (!post) {
-  //       return null;
-  //     }
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        if (err) {
+          console.log(`err`, err);
+          resolve(false);
+          return;
+        }
 
-  //     if (title) {
-  //       post.title = title;
-  //       await ctx.em.persistAndFlush(post);
-  //     }
-
-  //     return post;
-  //   }
-
-  //   @Mutation(() => Boolean)
-  //   async deletePost(@Arg("id") id: number, @Ctx() ctx: MyContext) {
-  //     await ctx.em.nativeDelete(Post, { id });
-  //     return true;
-  //   }
+        res.clearCookie(COOKIE_NAME);
+        resolve(true);
+      })
+    );
+  }
 }
